b38df788b8ec8a28700d3bf25859c24302e98b41
swcyu <yuseowonc@gmail.com>
2022-09-16 14:19:55 +0900

    220916-settings.py edit_#databases

--- pjt3_vegan_recipes/settings.py
+++ pjt3_vegan_recipes/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'pjt3_vegan_recipes.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
